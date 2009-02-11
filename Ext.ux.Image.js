3f8a4c937ff8551d01fad817353b63ff2b769019
chaluwa <chaluwa@a5bb0908-f7ed-11dd-a7be-4b3ef3b0700c>
2009-02-11 11:44:17 +0000

    extend proper superclass

--- Ext.ux.Image.js
+++ Ext.ux.Image.js
@@ -1,29 +1,29 @@
 Ext.namespace('Ext.ux');
-/**
- * Image component for ExtJs
- * 
- * @author Charles Opute Odili (chalu)
- * @version 1.0
- * @licence GPLv3
- */
 
 /**
  * @class Ext.ux.Image
  * @extends Ext.Component
- *
+ * @desc
+ * Image component for ExtJs
+ * 
+ * @licence GPLv3
+ * @author @author Charles Opute Odili (chalu) <a href="mailto:chaluwa@gmail.com">chaluwa@gmail.com</a>
+ * 
+ * <a href="http://extjs.com/forum/showthread.php?p=286538">Forum Thread</a>
+ * <a href="http://code.google.com/p/ext-ux-image/">Project Home</a>
+ * 
  * @constructor
  * @param {Object} config The config object
  */ 
-
-Ext.ux.Image = Ext.extend(Ext.Component, {
+Ext.ux.Image = Ext.extend(Ext.BoxComponent, {
 	/**
-	 * @param {String} src the URL of an image to initiaize it with, 
+	 * @cfg {String} src the URL of an image to initiaize it with, 
 	 * deafults to Ext.BLANK_IMAGE_URL
 	 */
 	src : Ext.BLANK_IMAGE_URL,
 	
 	/**
-	 * @private
+	 * @cfg
 	 */
 	autoEl : {
 		tag : 'img',
@@ -31,21 +31,34 @@ Ext.ux.Image = Ext.extend(Ext.Component, {
 		src : Ext.BLANK_IMAGE_URL		
 	},
 	
+	/**
+     * Inits this component with the specified config-properties and automatically
+     * creates its components.
+     */
 	initComponent: function () {		
 		Ext.ux.Image.superclass.initComponent.apply(this, arguments);
 	},
 	
+	/**
+     * Renders the component within it's container
+     */
 	onRender: function(){
 		Ext.ux.Image.superclass.onRender.apply(this, arguments);
 		
 		if(!Ext.isEmpty(this.src) && (this.src !== Ext.BLANK_IMAGE_URL)){
 			this.setSrc(this.src);
-		}		
+		}
+		this.relayEvents(this.el, 
+			[
+				"click", "dblclick", "mousedown", "mouseup", "mouseover",
+				"mousemove", "mouseout", "keypress", "keydown", "keyup"
+			]
+		);				
 	},
 	
 	/**
 	 * Sets the src for the image component
-	 * @param {String} src the new src
+	 * @cfg {String} src the new src
 	 */
 	setSrc: function (src) {
 		this.el.dom.src = src;
